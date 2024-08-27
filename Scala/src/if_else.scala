e9042eb215376ffec82ec2068f07269415f2f3e8
prabakaran <prabarajamani@gmail.com>
2024-08-27 15:38:51 +0530

    if else

---
+++ Scala/src/if_else.scala
@@ -0,0 +1,24 @@
+object if_else {
+
+
+  def main(arg: Array[String]): Unit = {
+    println("if_else start")
+    var fruit = "apple"
+    if (fruit == "apple")
+      println("apple")
+    else
+      println("no fruit")
+
+
+    var x = 10
+    if(x >= 80)
+      { println("Grade A") }
+    else if (x >= 65 && x <= 80)
+    println("grade B")
+    else if (x >= 50 && x <= 65)
+      println("grade C")
+    else if (x >= 35 && x <= 50)
+      println("grade D")
+    else println ("fail")
+  }
+}
