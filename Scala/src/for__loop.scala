772a69d7657c3c0df4f3e98199da3986dafd32f3
prabakaran <prabarajamani@gmail.com>
2024-08-27 23:57:41 +0530

    basics

--- Scala/src/for__loop.scala
+++ Scala/src/for__loop.scala
@@ -5,5 +5,80 @@ object for__loop {
     for (i <- 1 to 10) {
       println(i)
     }
+
+    for (i <- 1.to(5)) {
+      println("i using to "  + i)
+    }
+
+    for (i <- 1 until 5 ) {
+      println("i using until "  + i)
+    }
+
+    for (i <- 1 to 5; j <- 1 to 3 ) {
+      println("i using until "  + i + " j : " + j)
+    }
+
+    val lst = List(1,2,3,4,5);
+
+    for (i <- lst ){
+      println (" print using list " + i)
+    }
+
+    for (i <- lst; if i < 3 ){
+      println (" print using filter " + i)
+    }
+
+    val result = for {i <- lst; if i < 3 } yield
+    {
+      i*i
+    }
+
+    println("result : " + result)
   }
+
+  /*
+  output
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+  10
+  i using to 1
+  i using to 2
+  i using to 3
+  i using to 4
+  i using to 5
+  i using until 1
+  i using until 2
+  i using until 3
+  i using until 4
+  i using until 1 j : 1
+  i using until 1 j : 2
+  i using until 1 j : 3
+  i using until 2 j : 1
+  i using until 2 j : 2
+  i using until 2 j : 3
+  i using until 3 j : 1
+  i using until 3 j : 2
+  i using until 3 j : 3
+  i using until 4 j : 1
+  i using until 4 j : 2
+  i using until 4 j : 3
+  i using until 5 j : 1
+  i using until 5 j : 2
+  i using until 5 j : 3
+  print using list 1
+  print using list 2
+  print using list 3
+  print using list 4
+  print using list 5
+  print using filter 1
+  print using filter 2
+  result : List(1, 4)
+*/
 }
---
+++ Scala/src/match_expressions.scala
@@ -0,0 +1,35 @@
+object match_expressions {
+
+  def main(args: Array[String]): Unit = {
+
+    val age = "100";
+
+    age match {
+      case "18" => println(age);
+      case "20" => println(age);
+      case "30" => println(age);
+      case "40" => println(age);
+      case "50" => println(age);
+
+      case _ => println("No age requried");
+    }
+
+
+    val result = age match {
+      case "18" => age;
+      case "20" => age;
+      case "30" => age;
+      case "40" => age;
+      case "50" => age;
+
+      case _ => "default";
+    }
+
+    println("result = "+ result)
+    val i = 7;
+    i match {
+      case 1 | 3 | 5 | 7 | 9 => println("odd number");
+      case 2 | 4 | 6 | 8 | 10 => println("even number");
+    }
+  }
+}
